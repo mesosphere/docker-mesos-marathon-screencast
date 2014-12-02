06e08fb22cd76785adfb8adff02e16575d95086e
Ken Sipe <kensipe@gmail.com>
2014-12-02 01:25:13 -0600

    tomcat tarball moved

--- launch.sh
+++ launch.sh
@@ -4,4 +4,5 @@ if [ "$#" -ne 1 ]; then
     	echo "script takes json file as an argument"
 	exit 1;
 fi
-curl -X POST -H "Content-Type: application/json" 10.67.63.236:8080/v2/apps -d@"$@"
+
+curl -X POST -H "Content-Type: application/json" 10.74.209.40:8080/v2/apps -d@"$@"
