7066ac724eeecb35bbd4021485b2f470c92ca152
Ken Sipe <kensipe@gmail.com>
2014-12-02 01:32:56 -0600

    moving chronos scripts to another repo

--- launch-chronos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$#" -ne 1 ]; then
-    	echo "script takes json file as an argument"
-	exit 1;
-fi
-
-curl -i -L -H 'Content-Type: application/json' -X POST -d@"$@"  10.57.6.157:4400/scheduler/iso8601
